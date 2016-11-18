72783dabdf26668a14d13339e9a21eb2d05df05f
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-11-18 11:26:51 +0100

    release v0.0.4

--- RavuAlHemio.BarcodeSharp/Properties/AssemblyInfo.cs
+++ RavuAlHemio.BarcodeSharp/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -26,8 +25,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3")]
-[assembly: AssemblyFileVersion("0.0.3")]
+[assembly: AssemblyVersion("0.0.4")]
+[assembly: AssemblyFileVersion("0.0.4")]
 
 // internals visible to unit tests
 [assembly: InternalsVisibleTo("BarcodeSharpTests")]
