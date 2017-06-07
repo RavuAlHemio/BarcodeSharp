e9672c622847cb534029a82c69cce5b80bfb88d2
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2017-06-07 15:33:05 +0200

    have build process update assembly versions too

--- RavuAlHemio.BarcodeSharp/Properties/AssemblyInfo.cs
+++ RavuAlHemio.BarcodeSharp/Properties/AssemblyInfo.cs
@@ -5,12 +5,6 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RavuAlHemio.BarcodeSharp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RavuAlHemio.BarcodeSharp")]
-[assembly: AssemblyCopyright("Copyright \u00A9 2015 Ond\u0159ej Ho\u0161ek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -25,8 +19,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4")]
-[assembly: AssemblyFileVersion("0.0.4")]
 
 // internals visible to unit tests
 [assembly: InternalsVisibleTo("BarcodeSharpTests")]
